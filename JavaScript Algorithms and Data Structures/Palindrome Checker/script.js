f2e2cb6604a91289f385b2a8e62da48aaacad6a2
Covenant Ekojoe <150958017+Ekojoecovenant@users.noreply.github.com>
2025-01-20 02:14:30 +0100

    Completed Palindrome Checker
    I completed the design and structure for this project. Also added and edited its JavaScript code for better functionality. Fixed bugs. Removed unused or unneeded lines of codes.

--- JavaScript Algorithms and Data Structures/Palindrome Checker/script.js
+++ JavaScript Algorithms and Data Structures/Palindrome Checker/script.js
@@ -1,5 +1,28 @@
+function start() {
+  let enteredText = document.getElementById("text-input").value;
+  let output = document.getElementById("result");
+
+  if (enteredText === "") {
+    output.style.display = "block";
+    error(output);
+    output.innerText = "Please Input a Value";
+  } else {
+    let checked = palindrome(enteredText);
+    output.style.display = "block";
+
+    if (checked == true) {
+      output.innerText = isP(output, enteredText);
+    } else {
+      output.innerText = isNotP(output, enteredText);
+    }
+  }
+}
+
 function palindrome(str) {
   let alphaArr = str.match(/[a-zA-Z0-9]/g);
+  if (alphaArr === null) {
+    return true;
+  }
   return loop(alphaArr);
 }
 
@@ -25,6 +48,27 @@ function isLowerCase(string) {
   } else {
     return string.toLowerCase();
   }
+}
+
+// ERROR STYLE HANDLING
+
+function isP(value, checked) {
+  value.style.color = "white";
+  value.style.background = "hsl(219, 70%, 8%)";
+  //   value.style.background = "unset";
+  return `${checked} is a palindrome`;
+}
+
+function isNotP(value, checked) {
+  value.style.color = "white";
+  //   value.style.background = "unset";
+  value.style.background = "hsl(219, 70%, 8%)";
+  return `${checked} is not a palindrome`;
+}
+
+function error(value) {
+  value.style.color = "red";
+  value.style.background = "pink";
 }
 
 // Designed by Ekojoe Covenant Lemom AKA AlphaCode☯️
